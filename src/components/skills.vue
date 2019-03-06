69bec8cb71395e0d8ebc8777b6248eb80a914494
centimani <frostmoth@gmail.com>
2019-03-06 12:56:04 -0600

    Non SVG Skills WIP
    They have the right colors but the spacing isnt correct. Try to use the
    SVG TEXT feature instead but it wasn't centering correctly.

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -11,11 +11,25 @@
       align-h="center"
     >
       <h4 class="skillHeader">{{ cat.name }}</h4>
-      <b-col cols="4" lg="2" class="skillItem" v-for="skill in cat.skills" :key="skill.id">
+      <b-col
+        cols="4"
+        lg="2"
+        class="skillItem"
+        align-v="center"
+        v-for="skill in cat.skills"
+        :key="skill.id"
+      >
         <b-link :href="skill.url">
-          <svg viewBox="0 0 150 150" preserveAspectRatio="none">
+          <svg
+            viewBox="0 0 150 150"
+            :style="{backgroundColor:colors.mountain}"
+            v-if="skill.hasSvg === true"
+          >
             <path fill-rule="evenodd" :fill="colors.mountainSec" :d="skill.svg"></path>
           </svg>
+          <div v-else :style="{backgroundColor:colors.mountainSec, width:'100%' }" >
+            <span class="testspan" :style="{fontFamily: skill.font, color:colors.mountain}">{{skill.skill}}</span>
+          </div>
           <h6 v-bind:style="{ color: colors.text}">{{ skill.skill }}</h6>
         </b-link>
       </b-col>
@@ -41,10 +55,15 @@ export default {
 .skillItem {
   padding: 20px;
 }
-.skillItem h6{
-  padding: 20px;
+.skillItem a:hover {
+  color: white;
 }
-.skillItem a:hover{
-  color:white;
+.fontSkill {
+  height: 150px;
+}
+.testspan{
+  height:150px;
+  margin-bottom:0;
+  display: block;
 }
 </style>
